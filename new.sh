8f1651af8eedb5ab0e87a9c1df3c53f7640b2e2b
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-06-20 16:43:15 -0700

    Add readme creation to initialization

--- new.sh
+++ new.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
 NEW_DIR=$1
+
+if [ $# -ne 2 ]
+then
+    echo "Usage:   ./new.sh XXXX \"Problem Name\""
+    echo "Example: ./new.sh 0001 \"Two Sum\""
+    exit 1
+fi
+
+if [ -d $NEW_DIR ]
+then
+    echo "$NEW_DIR already exists!"
+    exit 1
+fi
+
 mkdir $NEW_DIR
 cp Makefile $NEW_DIR/Makefile
+echo "# $2" > $NEW_DIR/README.md
